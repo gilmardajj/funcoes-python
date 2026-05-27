47dbd98b6ee0445a2b83d1c64d9a423393d5a787
Rafael Souza <gilmardajj@gmail.com>
2026-05-27 19:33:32 -0300

    add exe02

---
+++ exe02.py
@@ -0,0 +1,9 @@
+def somar(a, b):
+    return a + b
+
+
+resultado1 = somar(2, 7)
+print(f"O resultado da soma é: {resultado1}")
+
+resultado2 = somar(5, 3)
+print(f"O resultado da soma é: {resultado2}")
