685a932ceda3495c98fce892fc0b6bf5d2b36a04
Rafael Souza <gilmardajj@gmail.com>
2026-05-27 19:58:57 -0300

    add exe04

---
+++ exe04.py
@@ -0,0 +1,13 @@
+def maior_numero(a, b):
+    if a > b:
+        print(f"O maior número é: {a}")
+    else:
+        print(f"O maior número é: {b}")
+
+# Exemplo de uso da função
+
+maior_numero(10, 5)
+
+# num1 = float(input("Digite o primeiro número: "))
+# num2 = float(input("Digite o segundo número: ")) 
+# maior_numero(num1, num2)
