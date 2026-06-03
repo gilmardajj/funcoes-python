efa73e2b0c95048f16919a46140b2caa19954252
Rafael Souza <gilmardajj@gmail.com>
2026-06-03 20:55:27 -0300

    EXE 06 AUTOMATIZADO

--- exe06.py
+++ exe06.py
@@ -6,12 +6,15 @@
 #         print(i)
 
 def contar_vogais(texto):
+    """Função que recebe um texto e retorna o número de vogais presentes nele."""
     contador = 0
     for letra in texto:
         if letra.lower() in "aeiou":
             contador += 1
     return contador
 
-texto = input("Digite um texto: ")
-numero_vogais = contar_vogais(texto)
-print(f"O número de vogais no texto é: {numero_vogais}")
+
+if __name__ == "__main__":
+    texto = input("Digite um texto: ")
+    numero_vogais = contar_vogais(texto)
+    print(f"O número de vogais no texto é: {numero_vogais}")
