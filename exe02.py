e2761e0cf359084eab17334cc0e045419e690ddc
Rafael Souza <gilmardajj@gmail.com>
2026-06-03 20:54:13 -0300

    EXE 02 AUTOMATIZADO

--- exe02.py
+++ exe02.py
@@ -1,9 +1,10 @@
 def somar(a, b):
+    """Função que recebe dois números e retorna a soma deles."""
     return a + b
 
+if __name__ == "__main__":
+    resultado1 = somar(2, 7)
+    print(f"O resultado da soma é: {resultado1}")
 
-resultado1 = somar(2, 7)
-print(f"O resultado da soma é: {resultado1}")
-
-resultado2 = somar(5, 3)
-print(f"O resultado da soma é: {resultado2}")
+    resultado2 = somar(5, 3)
+    print(f"O resultado da soma é: {resultado2}")
