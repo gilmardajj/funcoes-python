2a10c19e4f23e353972af6b60c131b66595b97df
Rafael Souza <gilmardajj@gmail.com>
2026-06-03 20:54:46 -0300

    EXE 04 AUTOMATIZADO

--- exe04.py
+++ exe04.py
@@ -1,12 +1,13 @@
 def maior_numero(a, b):
+    """Função que recebe dois números e retorna o maior entre eles."""
     if a > b:
         print(f"O maior número é: {a}")
     else:
         print(f"O maior número é: {b}")
 
 # Exemplo de uso da função
-
-maior_numero(10, 5)
+if __name__ == "__main__":
+    maior_numero(10, 5)
 
 # num1 = float(input("Digite o primeiro número: "))
 # num2 = float(input("Digite o segundo número: ")) 
